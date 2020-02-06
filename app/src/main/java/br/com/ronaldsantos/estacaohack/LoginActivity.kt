fced842dc6165aa8d8c0a3309b84c34a29eb76c8
Ronald Santos <ronaldsantos63@gmail.com>
2020-02-05 21:58:49 -0300

    Construcao de telas

---
+++ app/src/main/java/br/com/ronaldsantos/estacaohack/LoginActivity.kt
@@ -0,0 +1,12 @@
+package br.com.ronaldsantos.estacaohack
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
